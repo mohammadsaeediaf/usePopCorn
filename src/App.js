70204dde02e2627e705d6e77e1cfa6785ae80da7
VanGuardf <alisaadati18000@gmail.com>
2024-04-14 19:13:24 +0330

    Create component for each element of NavBar

--- src/App.js
+++ src/App.js
@@ -79,6 +79,44 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+function NumResult({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length} </strong> results
+    </p>
+  );
+}
+
+function NavBar({ children }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+}
 
 function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true);
