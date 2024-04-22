f3248ec08787c9cedd30e79dd468ee6ceadef769
unknown <alisaadati18000@gmail.com>
2024-04-22 22:09:49 +0330

    Add starRating file

---
+++ src/starRating.js
@@ -0,0 +1,28 @@
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+    display: "flex",
+    gap: "5px",
+}
+
+const textStyle={
+    lineHeightz: "1",
+    margin: "0",
+}
+
+export default function StarRating({maxRating = 5}) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>s{i + 1} </span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
+}
